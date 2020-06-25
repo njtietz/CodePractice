5cfe62e820b6328e64d31e27d619e1d8ee9c6cd6
njtietz <njtietz@gmail.com>
2020-06-25 11:37:17 -0400

    Create Friend RequestsIIWhoHastheMostFriends.sql

---
+++ Friend RequestsIIWhoHastheMostFriends.sql
@@ -0,0 +1,17 @@
+--https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/
+--mysql
+select ids as id, cnt as num
+from
+(
+select ids, count(*) as cnt
+   from
+   (
+        select requester_id as ids from request_accepted
+        union all
+        select accepter_id from request_accepted
+    ) as tbl1
+   group by ids
+   ) as tbl2
+order by cnt desc
+limit 1
+;
